ce519d75f16b05bfeef954ba50c61eb81b4a00ef
wendu24 <wendu2424@gmail.com>
2021-03-27 16:20:53 +0800

    第一次提交

---
+++ src/main/java/com/wendu/wjxapp/WjxAppApplication.java
@@ -0,0 +1,13 @@
+package com.wendu.wjxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjxAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wendu/wjxapp/WjxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendu.wjxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjxAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
